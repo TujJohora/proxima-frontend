4c1bf4141561314faa505637d0ece10b79cdca3f
TujJohora <tujjohoraa@gmail.com>
2023-03-05 11:48:33 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="app">
+      <h1 className="text-4xl">Hello Proxima</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter, sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
