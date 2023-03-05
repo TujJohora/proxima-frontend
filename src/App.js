5438a8e4fd8eec217a549d9a72e5624d71d7f0d1
TujJohora <tujjohoraa@gmail.com>
2023-03-05 17:14:41 +0600

    auth context and login, signup forms added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
 function App() {
   return (
     <div className="app bg-slate-900 text-slate-100 min-h-screen">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
       <Footer />
     </div>);
