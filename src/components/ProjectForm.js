c987bdc9b6e10db3428e4eb22e13e2a4eb346e25
TujJohora <tujjohoraa@gmail.com>
2023-03-05 15:54:56 +0600

    patch req added

--- src/components/ProjectForm.js
+++ src/components/ProjectForm.js
@@ -2,62 +2,100 @@ import { useState } from "react";
 import { useProjectsContext } from "../hooks/useProjectsContext";
 
 
-const ProjectForm = () => {
-  const [title, setTitle] = useState("");
-  const [tech, setTech] = useState("");
-  const [budget, setBudget] = useState("");
-  const [duration, setDuration] = useState("");
-  const [manager, setManager] = useState("");
-  const [dev, setDev] = useState("");
+const ProjectForm = ({ project, setIsModalOpen, setIsOverlayOpen }) => {
+  const [title, setTitle] = useState(project ? project.title : "");
+  const [tech, setTech] = useState(project ? project.tech : "");
+  const [budget, setBudget] = useState(project ? project.budget : "");
+  const [duration, setDuration] = useState(project ? project.duration : "");
+  const [manager, setManager] = useState(project ? project.manager : "");
+  const [dev, setDev] = useState(project ? project.dev : "");
+
   const [error, setError] = useState(null);
   const [emptyFields, setEmptyFields] = useState([]);
-
-  const { dispatch } = useProjectsContext();
-
-
+const { dispatch } = useProjectsContext();
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     // data
     const projectObj = { title, tech, budget, duration, manager, dev };
 
-    // post req
-    const res = await fetch("http://localhost:5000/api/projects", {
-      method: "POST",
+    // if there is no project, send post req
+    if (!project) {
+      // post req
+      const res = await fetch("http://localhost:5000/api/projects", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(projectObj),
+      });
+      const json = await res.json();
+// !res.ok, set error
+if (!res.ok) {
+  setError(json.error);
+  setEmptyFields(json.emptyFields);
+}
+
+// res.ok, reset
+if (res.ok) {
+  setTitle("");
+  setTech("");
+  setBudget("");
+  setDuration("");
+  setManager("");
+  setDev("");
+  setError(null);
+  setEmptyFields([]);
+  dispatch({ type: "CREATE_PROJECT", payload: json });
+}
+
+return;
+}
+// if there is a project, send patch req
+if (project) {
+  // send patch req
+  const res = await fetch(
+    `http://localhost:5000/api/projects/${project._id}`,
+    {
+      method: "PATCH",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(projectObj),
-    });
-    const json = await res.json();
-
-    // !res.ok, set error
-    if (!res.ok) {
-      setError(json.error);
-      setEmptyFields(json.emptyFields);
     }
+  );
+  const json = await res.json();
 
-    // res.ok, reset
-    if (res.ok) {
-      setTitle("");
-      setTech("");
-      setBudget("");
-      setDuration("");
-      setManager("");
-      setDev("");
-      setError(null);
-      setEmptyFields([]);
+  // !res.ok
+  if (!res.ok) {
+    setError(json.error);
+    setEmptyFields(json.emptyFields);
+  }
 
-      dispatch({ type: "CREATE_PROJECT", payload: json });
-    }
-  };
+  // res.ok
+  if (res.ok) {
+    setError(null);
+    setEmptyFields([]);
 
-  return (
-    <form onSubmit={handleSubmit} className="project-form flex flex-col gap-5">
-      <h2 className="text-4xl font-medium text-sky-400 mb-10">
+    // dispatch
+    dispatch({ type: "UPDATE_PROJECT", payload: json });
+
+    // close overlay and modal
+    setIsModalOpen(false);
+    setIsOverlayOpen(false);
+  }
+
+  return;  }
+};
+
+return (
+  <form onSubmit={handleSubmit} className="project-form flex flex-col gap-5">
+    <h2
+        className={`text-4xl font-medium text-sky-400 mb-10 ${
+          project ? "hidden" : ""
+        }`}
+      >
         Add a New Project
       </h2>
-
       <div className="form-control flex flex-col gap-2">
         <label
           htmlFor="title"
@@ -78,7 +116,7 @@ const ProjectForm = () => {
           }`}
         />
       </div>
-
+      
       <div className="form-control flex flex-col gap-2">
         <label
           htmlFor="tech"
@@ -98,8 +136,7 @@ const ProjectForm = () => {
               : "border-slate-500"
           }`}
         />
-      </div>
-
+         </div>
       <div className="form-control flex flex-col gap-2">
         <label
           htmlFor="budget"
@@ -118,17 +155,12 @@ const ProjectForm = () => {
               ? "border-rose-500"
               : "border-slate-500"
           }`}
-        />
+          />
       </div>
-
       <div className="form-control flex flex-col gap-2">
         <label
           htmlFor="duration"
-          className={`bg-transparent border  py-3 px-5 rounded-lg outline-none focus:border-sky-400 duration-300 ${
-            emptyFields.includes("duration")
-              ? "border-rose-500"
-              : "border-slate-500"
-          }`}
+          className="cursor-pointer hover:text-sky-400 duration-300"
         >
           Duration (in weeks)
         </label>
@@ -138,63 +170,58 @@ const ProjectForm = () => {
           type="number"
           placeholder="e.g. e-commerce website"
           id="duration"
-          className="bg-transparent border border-slate-500 py-3 px-5 rounded-lg outline-none focus:border-sky-400 duration-300"
-        />
-      </div>
-
-      <div className="form-control flex flex-col gap-2">
-        <label
-          htmlFor="manager"
-          className="cursor-pointer hover:text-sky-400 duration-300"
-        >
-          Manager
-        </label>
-        <input
-          value={manager}
-          onChange={(e) => setManager(e.target.value)}
-          type="text"
-          placeholder="e.g. natasha"
-          id="manager"
           className={`bg-transparent border  py-3 px-5 rounded-lg outline-none focus:border-sky-400 duration-300 ${
-            emptyFields.includes("manager")
+            emptyFields.includes("duration")
               ? "border-rose-500"
-              : "border-slate-500"
-          }`}
-        />
-      </div>
-
-      <div className="form-control flex flex-col gap-2">
-        <label
-          htmlFor="dev"
-          className="cursor-pointer hover:text-sky-400 duration-300"
-        >
-          Developers
-        </label>
-        <input
-          value={dev}
-          onChange={(e) => setDev(e.target.value)}
-          type="number"
-          placeholder="e.g. 5"
-          id="dev"
-          className={`bg-transparent border  py-3 px-5 rounded-lg outline-none focus:border-sky-400 duration-300 ${
-            emptyFields.includes("dev") ? "border-rose-500" : "border-slate-500"
-          }`}
-        />
-      </div>
-
-      <button
-        type="submit"
-        className="bg-sky-400 text-slate-900 py-3 rounded-lg hover:bg-sky-50 duration-300"
-      >
-        Add Project
-      </button>
-      {error && (
-        <p className="bg-rose-500/20 rounded-lg p-5 text-rose-500 border border-rose-500">
-          {error}
-        </p>
-      )}
-    </form>
-  );
-};
-
-export default ProjectForm;
+              : "border-slate-500"    }`}
+              />
+            </div>
+            <div className="form-control flex flex-col gap-2">
+              <label
+                htmlFor="manager"
+                className="cursor-pointer hover:text-sky-400 duration-300"
+              >
+                Manager
+              </label>
+              <input
+                value={manager}
+                onChange={(e) => setManager(e.target.value)}
+                type="text"
+                placeholder="e.g. natasha"
+                id="manager"
+                className={`bg-transparent border  py-3 px-5 rounded-lg outline-none focus:border-sky-400 duration-300 ${
+                  emptyFields.includes("manager")
+                    ? "border-rose-500": "border-slate-500"
+                  }`}
+                />
+              </div>
+              <div className="form-control flex flex-col gap-2">
+                <label
+                  htmlFor="dev"
+                  className="cursor-pointer hover:text-sky-400 duration-300"
+                >
+                  Developers
+                </label>
+                <input
+                  value={dev}
+                  onChange={(e) => setDev(e.target.value)}
+                  type="number"
+                  placeholder="e.g. 5"
+                  id="dev"
+                  className={`bg-transparent border  py-3 px-5 rounded-lg outline-none focus:border-sky-400 duration-300 ${
+                    emptyFields.includes("dev") ? "border-rose-500" : "border-slate-500"  }`}
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className="bg-sky-400 text-slate-900 py-3 rounded-lg hover:bg-sky-50 duration-300"
+                  > {project ? "Confirm Update" : "Add Project"}
+                  </button>
+                  {error && (
+                    <p className="bg-rose-500/20 rounded-lg p-5 text-rose-500 border border-rose-500">{error}
+                    </p>
+                  )}
+                </form>
+              );
+            };
+            export default ProjectForm;
